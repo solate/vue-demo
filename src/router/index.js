bf86b1cdd126ec55c2a6f1dc66dccf611930851d
solate <349370626@qq.com>
2021-06-20 16:54:39 +0800

    vue-router

--- src/router/index.js
+++ src/router/index.js
@@ -1,26 +1,56 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Home from "../views/Home.vue";
+import store from "@/store/index"; //引入状态管理
+import NProgress from "nprogress"; //引入进度条组件 cnpm install nprogress --save
+import "nprogress/nprogress.css";
+
+/**
+ *@parma {String} name 文件夹名称
+ *@parma {String} component 视图组件名称
+ */
+const getComponent = (name, component) => () =>
+  import(`@/views/${name}/${component}.vue`);
 
 const routes = [
   {
     path: "/",
-    name: "Home",
-    component: Home,
+    redirect: "/home",
+    component: getComponent("login", "index"),
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    path: "/login",
+    name: "login",
+    component: getComponent("login", "index"),
+  },
+  {
+    path: "/",
+    component: getComponent("layout", "layout"),
+    children: [
+      {
+        path: "/home",
+        name: "home",
+        component: getComponent("home", "index"),
+        meta: { title: "首页" },
+      },
+    ],
   },
 ];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+});
+
+//判断是否存在token
+router.beforeEach((to, from, next) => {
+  NProgress.start();
+  if (to.path !== "/login" && !store.state.token) {
+    next("/login"); //跳转登录
+    NProgress.done(); // 结束Progress
+  }
+  next();
+});
+router.afterEach(() => {
+  NProgress.done(); // 结束Progress
 });
 
 export default router;
